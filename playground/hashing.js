1c00aa558c2052b6bca39ef4bb8152b1f4a50191
Udayasuryan Kodoly <udayasurya007@gmail.com>
2017-09-02 09:40:28 -0700

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,28 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
-var token = jwt.sign(data,'123abc');
-console.log(token);
+var password = '123abc!'
 
-var decoded = jwt.verify(token, '123abc')
-console.log('decoded:', decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//       console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$PvddFCiAT6ET1UmzPzUwPOkX0bI/xNHh7.5v2Wp8YdylEZv47F/hm'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+})
+// var data = {
+//   id: 10
+// };
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc')
+// console.log('decoded:', decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
