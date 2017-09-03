d95875fac7d49e5ec5e0e9e8abc9a04035864fea
Udayasuryan Kodoly <udayasurya007@gmail.com>
2017-09-03 15:39:05 -0700

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,15 @@ UserSchema.methods.generateAuthToken = function (){
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.statics.findByCredentials = function (email, password) {
   var user = this;
 
